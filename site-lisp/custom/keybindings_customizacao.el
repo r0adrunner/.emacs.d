0a94fa3b5fc358d6bad3d763b802b66acbb90249
Victor Moreira <victorpolillo@hotmail.com>
2011-03-03 11:09:56 -0300

    Ctrl-tab funciona no org-mode.

--- site-lisp/custom/keybindings_customizacao.el
+++ site-lisp/custom/keybindings_customizacao.el
@@ -6,3 +6,7 @@
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (define-key org-mode-map [(control tab)] nil)))

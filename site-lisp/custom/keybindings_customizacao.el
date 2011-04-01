6c8a224d162e52436487adcccded278a01f055ad
Victor Moreira <victorpolillo@hotmail.com>
2011-04-01 15:21:38 -0300

    Rem VIM keybindings, add copiar e colar de programas externos, mods menores.

--- site-lisp/custom/keybindings_customizacao.el
+++ site-lisp/custom/keybindings_customizacao.el
@@ -1,3 +1,9 @@
+;; make cursor movement keys under right hand's home-row.
+(global-set-key (kbd "M-i") 'previous-line) ; was tab-to-tab-stop
+(global-set-key (kbd "M-j") 'backward-char) ; was indent-new-comment-line
+(global-set-key (kbd "M-k") 'next-line) ; was kill-sentence
+(global-set-key (kbd "M-l") 'forward-char)  ; was downcase-word
+
 (global-set-key (kbd "<C-tab>") 'other-window)
 (iswitchb-mode t)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -10,3 +16,38 @@
 (add-hook 'org-mode-hook
           '(lambda ()
              (define-key org-mode-map [(control tab)] nil)))
+
+;; Mover linhas p cima e p baixo estilo Eclipse
+(defun move-text-internal (arg) 
+  (cond 
+   ((and mark-active transient-mark-mode) 
+    (if (> (point) (mark)) 
+        (exchange-point-and-mark)) 
+    (let ((column (current-column)) 
+          (text (delete-and-extract-region (point) (mark)))) 
+      (forward-line arg) 
+      (move-to-column column t) 
+      (set-mark (point)) 
+      (insert text) 
+      (exchange-point-and-mark) 
+      (setq deactivate-mark nil))) 
+   (t 
+    (beginning-of-line) 
+    (when (or (> arg 0) (not (bobp))) 
+      (forward-line) 
+      (when (or (< arg 0) (not (eobp))) 
+        (transpose-lines arg)) 
+      (forward-line -1)))))
+(defun move-text-down (arg) 
+  "Move region (transient-mark-mode active) or current line 
+  arg lines down." 
+  (interactive "*p") 
+  (move-text-internal arg))
+(defun move-text-up (arg) 
+  "Move region (transient-mark-mode active) or current line 
+  arg lines up." 
+  (interactive "*p") 
+  (move-text-internal (- arg)))
+
+(global-set-key (kbd "<M-up>") 'move-text-up)
+(global-set-key (kbd "<M-down>") 'move-text-down)
--- site-lisp/init_comum.el
+++ site-lisp/init_comum.el
@@ -1,12 +1,14 @@
 (require 'color-theme)
 (setq color-theme-load-all-themes nil)
 
+;; Usar hard-links sem problemas com o esquema de backups do emacs
+(setq backup-by-copying-when-linked t)
 
 (load "autosaves e backups.el")  ;; Poe todos os autosaves e backups em um diretorio separado
 (load "org-mode_customizacao")
 (load "keybindings_customizacao.el")
 (load "full-ack_customizacao.el")
-(load "vim-mode_customizacao.el")
+;;(load "vim-mode_customizacao.el")
 (require 'org-depend)
 
 ;; Habilita o server para abrir arquivos na mesma janela
@@ -15,5 +17,9 @@
 ;; Desabilita o toolbar
 (tool-bar-mode -1)
 
+;; Permite copiar e colar de outros programas.
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+;; Normal = 400
+(setq max-list-eval-depth 1000)

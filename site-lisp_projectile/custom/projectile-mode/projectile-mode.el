bb2e3f4cb9db8c3328bd4b98b4a212b27f18b5b6
Victor Moreira <victorpolillo@hotmail.com>
2011-04-04 19:03:35 -0300

    Mode-line do projectile com cores, bugs corrigidos.

--- site-lisp_projectile/custom/projectile-mode/projectile-mode.el
+++ site-lisp_projectile/custom/projectile-mode/projectile-mode.el
@@ -13,6 +13,11 @@
        (interactive) 
        (find-file "~/develop/workspace/projectile/beta/versions/standard/copy/config/Captions.txt")))
 
+    (,(kbd "C-ç s r") . 
+     (lambda () 
+       (interactive) 
+       (projectile-restart)))
+
     )
   ;; Make mode global rather than buffer local
   :global 1)
@@ -36,50 +41,55 @@
 
 ;; Filtro para o output do compilation buffer.
 (add-hook 'comint-output-filter-functions 
-     '(lambda(txt) 
-	(progn
-	  (if (and (string-match "INFO: Stopping Coyote" txt) projectile-deve-reiniciar)
-	      (progn 
-		;;(sleep-for 1)
-		(delete-process (get-buffer-process (current-buffer)) )
-		(setq projectile-deve-reiniciar nil)
-		(save-window-excursion (projectile-start))
-		;;(delete-windows-on (get-buffer "*Projectile-server-run*")
-		))
-	  (if (string-match "INFO: Server startup in" txt)
-	      (progn (message "Projectile foi iniciado.") 
-		     ;;(delete-windows-on (get-buffer "*Projectile-server-run*"))
-		     )))))
+	  '(lambda(txt) 
+	     (cond ((string-match "INFO: Stopping Coyote" txt)
+		    ;; Projectile parou:
+		    (projectile-muda-status "inativo"))
+		   ;; Projectile iniciou:
+		   ((string-match "INFO: Server startup in" txt) 
+		    (projectile-muda-status "ativo")))))
 
 (defun projectile-start ()
   "Função para começar o Projectile."
   (interactive)
-  (let ((default-directory "~/develop/workspace/projectile/")
-	(compilation-buffer-name-function
-         (function
-          (lambda(ign)
-            "*Projectile-server-run*"))))
-    (compilation-start "dist/projectile" t)))
+  (let ((default-directory "/home/victor/develop/workspace/projectile/"))
+    (switch-to-buffer (get-buffer-create "*Projectile-server-run*"))
+    (comint-mode)
+    (compilation-minor-mode t)
+    (projectile-muda-status "carregando")
+    (comint-exec "*Projectile-server-run*" "projectile server" "/home/victor/develop/workspace/projectile/dist/projectile" nil nil)
+   (set-process-sentinel (get-process "projectile server") 'projectile-process-sentinel)))
+
+(defun projectile-process-sentinel (process event)
+  "Process sentinel p o pjt server"
+  (when (and projectile-deve-reiniciar (string= event "interrupt\n")) 
+    ;; Deve reiniciar?
+    (progn
+      (setq projectile-deve-reiniciar nil)
+      (save-window-excursion (projectile-start)))))
 
 (defun projectile-stop ()
   "Manda um sinal de interrupt para o processo do projectile"
   (interactive)
+  (projectile-muda-status "carregando")
   (set-buffer "*Projectile-server-run*")
   (comint-interrupt-subjob))
 
 (defun projectile-restart ()
   "Reinicia o servidor que roda o projectile"
   (interactive)
-  (set-buffer "*Projectile-server-run*")
-  (if (not (get-buffer-process (current-buffer)))
-      (projectile-start)
-    (progn (setq projectile-deve-reiniciar t)
-	   (projectile-stop))))
+  (save-window-excursion
+    (switch-to-buffer (get-buffer-create "*Projectile-server-run*"))
+    (if (not (get-buffer-process (current-buffer)))
+	(projectile-start)
+      (progn (setq projectile-deve-reiniciar t)
+	     (projectile-muda-status "carregando")
+	     (projectile-stop)))))
 
 (defun projectile-bsm-build ()
   "Função para chamar a função JDE ant build com devido default-directory."
   (interactive)
-  (let ((default-directory "~/develop/workspace/bsm/")
+  (let ((default-directory "/home/victor/develop/workspace/bsm/")
 	(jde-build-function '(jde-ant-build))
 	(jde-ant-buildfile "build_victor.xml")
 	(jde-ant-args "-emacs projectile"))
@@ -88,7 +98,7 @@
 (defun projectile-build ()
   "Função para chamar a função JDE ant build com devido default-directory."
   (interactive)
-  (let ((default-directory "~/develop/workspace/projectile/")
+  (let ((default-directory "/home/victor/develop/workspace/projectile/")
 	(jde-build-function '(jde-ant-build))
 	(jde-ant-buildfile "build_victor.xml")
 	(jde-ant-args (format "-emacs -Dversion=%s clean dist" projectile-versao-atual))
@@ -101,3 +111,30 @@
             "*Projectile ant build*"))))
     (jde-build)))
 
+(defun projectile-muda-status (status)
+  "Muda a mode-line de acordo com o status do servidor do Projectile"
+  (interactive)
+  (setq minor-mode-alist (assq-delete-all 'projectile-mode minor-mode-alist))
+  (cond
+   ((string= status "ativo") 
+    (setq minor-mode-alist (cons '(projectile-mode #(" PJT" 0 3 (face ativo))) minor-mode-alist)))
+   ((string= status "inativo") 
+    (setq minor-mode-alist (cons '(projectile-mode #(" PJT" 0 3 (face inativo))) minor-mode-alist)))
+   ((string= status "carregando") 
+    (setq minor-mode-alist (cons '(projectile-mode #(" PJT" 0 3 (face carregando))) minor-mode-alist)))))
+
+;; Cores de fonte para o mode-line "PJT"
+(defface ativo
+  `((t (:foreground "green" :weight bold)))
+  "Face usada quando o server do projectile está ativo.")
+
+(defface carregando
+  `((t (:foreground "yellow" :weight bold)))
+  "Face usada quando o server do projectile está ativo.")
+
+(defface inativo
+  `((t (:foreground "red" :weight bold)))
+  "Face usada quando o server do projectile está ativo.")
+
+;; Inicia a mode-line com o PJT inativo
+(projectile-muda-status "inativo")

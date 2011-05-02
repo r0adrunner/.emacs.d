4d2f990e7a78c40d1afda94ebdeedd735a91eef6
Victor Moreira <victorpolillo@hotmail.com>
2011-05-02 15:30:17 -0300

    Func. marcar e limpar na captions-mode: f_funcoes.el => captions-mode.el

--- site-lisp_projectile/custom/f_funcoes.el
+++ site-lisp_projectile/custom/f_funcoes.el
@@ -1,15 +1,10 @@
-(defun custfn_f9 (str)
-  (interactive (list (read-from-minibuffer "String a marcar: " (replace-regexp-in-string "\\$$" "" (car (if isearch-regexp regexp-search-ring search-ring))))))
-  (save-excursion
-  (beginning-of-buffer)
-  (replace-regexp (concat str "$") (quote (replace-eval-replacement concat "\\&_-DelMe_-" (number-to-string replace-count))))))
+(defun custfn_f9 ()
+  (interactive)
+  )
 
 (defun custfn_f10 ()
   (interactive)
-  (save-excursion
-  (beginning-of-buffer)
-  (replace-regexp "_-DelMe_-[0-9]+" "")))
+  )
 
 (global-set-key (kbd "<f9>") 'custfn_f9)
 (global-set-key (kbd "<f10>") 'custfn_f10)
-  
--- site-lisp_projectile/custom/projectile-mode/captions-mode.el
+++ site-lisp_projectile/custom/projectile-mode/captions-mode.el
@@ -2,6 +2,27 @@
 
 (add-to-list 'auto-mode-alist '("config/Captions\\.txt\\'" . captions-mode))
 
+(defvar captions-mode-map nil "Keymap para o captions-mode")
+
+(when (not captions-mode-map) ; if it is not already defined
+  ;; assign your keyboard shortcuts
+  (setq captions-mode-map (make-sparse-keymap))
+  (define-key captions-mode-map (kbd "C-c m") 'captions-mode-marcar)
+  (define-key captions-mode-map (kbd "C-c i") 'captions-mode-limpar))
+
+(defun captions-mode-marcar (str)
+  (interactive (list (read-from-minibuffer "String a marcar: " (replace-regexp-in-string "\\$$" "" (concat (car (if isearch-regexp regexp-search-ring search-ring)) "")))))
+  (save-excursion
+  (beginning-of-buffer)
+  (replace-regexp (concat str "$") (quote (replace-eval-replacement concat "\\&_-DelMe_-" (number-to-string replace-count))))))
+
+(defun captions-mode-limpar ()
+  (interactive)
+  (save-excursion
+  (beginning-of-buffer)
+  (replace-regexp "_-DelMe_-[0-9]+" "")))
+
+
 ;; captions-mode major mode 
 (define-derived-mode captions-mode outline-mode
   (setq mode-name "Captions")

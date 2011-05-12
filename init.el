c9ff406545dd3d742dc4d57bb7367d4ef262241d
Victor Moreira <victorpolillo@hotmail.com>
2011-05-12 17:55:54 -0300

    Remove a toolbar e alteração da fonte.

--- init.el
+++ init.el
@@ -16,10 +16,12 @@
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
  '(inhibit-startup-screen t)
  '(org-enforce-todo-dependencies t)
- '(org-track-ordered-property-with-tag "ORDENADO"))
+ '(org-track-ordered-property-with-tag "ORDENADO")
+ '(tool-bar-mode nil))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))

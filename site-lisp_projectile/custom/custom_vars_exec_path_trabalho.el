fdd91177c02f45eddcc7b1ffcb414ed97709bf0c
Victor <victor.moreira@projectile.com.br>
2011-05-20 12:01:33 -0300

    Add Env. variable "PATH" para o path do emacsclient.

--- site-lisp_projectile/custom/custom_vars_exec_path_trabalho.el
+++ site-lisp_projectile/custom/custom_vars_exec_path_trabalho.el
@@ -5,6 +5,7 @@
 	     "/home/victor/develop/system/maven/bin" ":"
 	     "/usr/local/apache-maven-3.0.1/bin" ":"
 	     "/usr/local/sbin" ":"
+	     "/usr/local/bin/" ":"
 	     "/usr/sbin" ":"
 	     "/usr/bin" ":"
 	     "/sbin" ":"

484b73f50682a858c64240b8e7a0d54a0e2b3d08
Victor <victor.moreira@projectile.com.br>
2011-08-29 19:31:50 -0300

    Add função de reminder para o org-mode.

--- site-lisp/custom/org-mode_customizacao.el
+++ site-lisp/custom/org-mode_customizacao.el
@@ -8,8 +8,7 @@
             (org-indent-mode t))
           t)
 
-
-
+;; --------------------------------------------------
 (setq org-todo-keywords
       '((sequence "TODO(t)" "|" "FEITO(f)" "CANCELADO(c)")
 	))
@@ -18,11 +17,31 @@
       '( ("CANCELADO" . (:foreground "white" :background "#777700" :weight bold))
 	 ))
 
+;; --------------------------------------------------
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "FEITO" "TODO"))))
 
-     (defun org-summary-todo (n-done n-not-done)
-       "Switch entry to DONE when all subentries are done, to TODO otherwise."
-       (let (org-log-done org-log-states)   ; turn off logging
-         (org-todo (if (= n-not-done 0) "FEITO" "TODO"))))
-     
-     (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
+;; --------------------------------------------------
+;; De: http://emacs-fu.blogspot.com/2009/11/showing-pop-ups.html
+
+;; the appointment notification facility
+(setq
+ appt-message-warning-time 30 ;; warn x min in advance
+ appt-display-interval 15     ;; Checa a cada x minutos
+ appt-display-mode-line t
+ appt-display-format 'window) ;; use our func
+
+(run-at-time  nil (* 60 60 1) 'org-agenda-to-appt 'refresh) ;; Roda org-agenda-to-appt regularmente
+
+;; our little façade-function for notify-popup-zenity
+(defun my-appt-display (min-to-app new-time msg)
+  (notify-popup-zenity "Alerta" (concat (format "Evento em %s minutos:\n\n" min-to-app) msg)
+		       "/usr/share/icons/gnome/32x32/status/appointment-soon.png"
+		       "/usr/share/sounds/ubuntu/stereo/phone-incoming-call.ogg"))
+
+(setq appt-disp-window-function (function my-appt-display))
+(appt-activate 1)              ;; active appt (appointment notification)

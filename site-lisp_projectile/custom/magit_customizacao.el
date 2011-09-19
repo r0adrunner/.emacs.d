9882a7a45ff3164d3a6a59defd537b2f8cd762e9
Victor <victor.moreira@projectile.com.br>
2011-09-19 10:52:02 -0300

    Add pentaho e .emacs.d à lista magit-repo-dirs.

--- site-lisp_projectile/custom/magit_customizacao.el
+++ site-lisp_projectile/custom/magit_customizacao.el
@@ -1,1 +1,1 @@
-(setq magit-repo-dirs (quote ("/home/victor/develop/workspace/bsm" "/home/victor/develop/workspace/projectile")))
+(setq magit-repo-dirs (quote ("/home/victor/develop/workspace/bsm" "/home/victor/.emacs.d" "/home/victor/develop/workspace/projectile" "/home/victor/develop/workspace/pentaho")))

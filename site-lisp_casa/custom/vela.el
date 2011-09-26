0f0366deb7c75e9bc6a3486ea814d0ca59cdecc5
Victor <victor.moreira@projectile.com.br>
2011-09-26 09:11:05 -0300

    Add. scripts agua.el e vela.el

---
+++ site-lisp_casa/custom/vela.el
@@ -0,0 +1,5 @@
+(defun timer-vela ()
+(interactive)
+(progn
+  (sleep-for (* 60 12))
+  (set-background-color "yellow")))
--- site-lisp_casa/init_casa.el
+++ site-lisp_casa/init_casa.el
@@ -1,1 +1,3 @@
 (load "org_customizacao_casa.el")
+(load "agua.el")
+(load "vela.el")

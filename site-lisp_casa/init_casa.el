db61449aee65f8564717c64c8992c9358da1e3be
Victor Moreira <victorpolillo@hotmail.com>
2011-12-06 06:40:49 -0200

    Haskell mode

--- site-lisp_casa/init_casa.el
+++ site-lisp_casa/init_casa.el
@@ -6,13 +6,22 @@
 ;; Color themes ---------------------------------------------------------
 
 ;;(load "tangotango_init.el")
- 
+
 (load "color-theme-zenburn.el")
 (color-theme-zenburn)
 
 ;; Color theme init:
-;(require 'color-theme)
-;(setq color-theme-is-global t)
-;(color-theme-initialize)
+;;(require 'color-theme)
+;;(setq color-theme-is-global t)
+;;(color-theme-initialize)
 
-;(color-theme-gray1)
+;;(color-theme-gray1)
+
+;; Haskell mode -------------------------------------------------------
+
+(load "/usr/share/emacs/site-lisp/haskell-mode/haskell-site-file")
+
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)

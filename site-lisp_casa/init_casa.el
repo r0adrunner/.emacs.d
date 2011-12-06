142da576965a2a4de8b374de6e951c2b22c69eb2
Victor Moreira <victorpolillo@hotmail.com>
2011-12-06 03:43:49 -0200

    Usado zenburn no profile de casa.

--- site-lisp_casa/init_casa.el
+++ site-lisp_casa/init_casa.el
@@ -1,3 +1,17 @@
 (load "org_customizacao_casa.el")
 (load "agua.el")
 (load "vela.el")
+
+;; Color themes ---------------------------------------------------------
+
+;;(load "tangotango_init.el")
+ 
+(load "color-theme-zenburn.el")
+(color-theme-zenburn)
+
+;; Color theme init:
+;(require 'color-theme)
+;(setq color-theme-is-global t)
+;(color-theme-initialize)
+
+;(color-theme-gray1)

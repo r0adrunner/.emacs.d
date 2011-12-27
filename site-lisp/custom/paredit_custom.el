34cdac91c93c0a7a8c568aeefbdae10e8afe682d
Victor Moreira <victorpolillo@hotmail.com>
2011-12-27 20:11:53 -0200

    Add. rainbow delimiters

--- site-lisp/custom/paredit_custom.el
+++ site-lisp/custom/paredit_custom.el
@@ -2,5 +2,7 @@
   "Minor mode for pseudo-structurally editing Lisp code." t)
 
 (defun turn-on-paredit ()
-  (paredit-mode +1))
+  (paredit-mode +1)
+  (when (require 'rainbow-delimiters nil 'noerror) 
+    (rainbow-delimiters-mode)))
 

a6fd69558e136cb0ab323718d896b20fc2513eb3
Victor Moreira <victorpolillo@hotmail.com>
2011-12-27 19:53:12 -0200

    Rem M-up e M-down para mover linhas estilo eclipse.

--- site-lisp/custom/keybindings_customizacao.el
+++ site-lisp/custom/keybindings_customizacao.el
@@ -43,40 +43,6 @@
      (global-set-key (kbd "s-k")   'pager-row-down)
 
 
-;; MOVER linhas p cima e p baixo estilo Eclipse
-(defun move-text-internal (arg) 
-  (cond 
-   ((and mark-active transient-mark-mode) 
-    (if (> (point) (mark)) 
-        (exchange-point-and-mark)) 
-    (let ((column (current-column)) 
-          (text (delete-and-extract-region (point) (mark)))) 
-      (forward-line arg) 
-      (move-to-column column t) 
-      (set-mark (point)) 
-      (insert text) 
-      (exchange-point-and-mark) 
-      (setq deactivate-mark nil))) 
-   (t 
-    (beginning-of-line) 
-    (when (or (> arg 0) (not (bobp))) 
-      (forward-line) 
-      (when (or (< arg 0) (not (eobp))) 
-        (transpose-lines arg)) 
-      (forward-line -1)))))
-(defun move-text-down (arg) 
-  "Move region (transient-mark-mode active) or current line 
-  arg lines down." 
-  (interactive "*p") 
-  (move-text-internal arg))
-(defun move-text-up (arg) 
-  "Move region (transient-mark-mode active) or current line 
-  arg lines up." 
-  (interactive "*p") 
-  (move-text-internal (- arg)))
-(global-set-key (kbd "<M-up>") 'move-text-up)
-(global-set-key (kbd "<M-down>") 'move-text-down)
-
 ;;========================= C-j = backspace C-o = C-j
 ;; Melhora atalhos para backspace e newline. (Global)
 (global-set-key "\C-o" 'newline-and-indent)

079a99fcf1f5d8ae10a8fdedaf6ffb33c538b7ed
Victor Moreira <victorpolillo@hotmail.com>
2012-02-09 18:22:58 -0200

    org-agenda-custom-commands melhorado

--- site-lisp_casa/custom/org_customizacao_casa.el
+++ site-lisp_casa/custom/org_customizacao_casa.el
@@ -3,9 +3,17 @@
 ;; Org custom commands
 (setq org-agenda-custom-commands
       '(("o" "Todo melhorado"
-         ((agenda "" ((org-agenda-ndays 7))) ;; review upcoming deadlines and appointments
-                                           ;; type "l" in the agenda to review logged items 
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/site.org")) (org-agenda-overriding-header "SITE:")))
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/victor.org")) (org-agenda-overriding-header "GERAIS:"))))) ;; review waiting items 
-         ;; ...other commands here
+	 ;; review upcoming deadlines and appointments
+	 ;; type "l" in the agenda to review logged items
+         ((agenda "" ((org-agenda-ndays 10)
+		      (org-agenda-repeating-timestamp-show-all t)
+		      (org-agenda-start-on-weekday nil)
+		      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("FEITO")))))
+	  ;; review waiting items
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/site.org"))
+							      (org-agenda-overriding-header "SITE:")))
+	  ;; review waiting items							      
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/victor.org"))
+							      (org-agenda-overriding-header "GERAIS:")))))
+	;; ...other commands here
         ))

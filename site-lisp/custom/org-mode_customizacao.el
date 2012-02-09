7b3bef995d7b3d46323dc6c03a06e6d959d80aa7
Victor Moreira <victorpolillo@hotmail.com>
2012-02-09 17:07:05 -0200

    Movido org custom. C-c a o para arquivo de custom. casa

--- site-lisp/custom/org-mode_customizacao.el
+++ site-lisp/custom/org-mode_customizacao.el
@@ -35,16 +35,6 @@
 
 (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
-;; Org custom commands
-(setq org-agenda-custom-commands
-      '(("o" "Todo melhorado"
-         ((agenda "" ((org-agenda-ndays 7))) ;; review upcoming deadlines and appointments
-                                           ;; type "l" in the agenda to review logged items 
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/site.org")) (org-agenda-overriding-header "SITE:")))
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/victor.org")) (org-agenda-overriding-header "GERAIS:"))))) ;; review waiting items 
-         ;; ...other commands here
-        ))
-
 ;; --------------------------------------------------
 ;; De: http://emacs-fu.blogspot.com/2009/11/showing-pop-ups.html
 
--- site-lisp_casa/custom/org_customizacao_casa.el
+++ site-lisp_casa/custom/org_customizacao_casa.el
@@ -1,1 +1,11 @@
 (setq org-agenda-files (quote ("~/arquivos/org/victor.org" "~/arquivos/org/site.org")))
+
+;; Org custom commands
+(setq org-agenda-custom-commands
+      '(("o" "Todo melhorado"
+         ((agenda "" ((org-agenda-ndays 7))) ;; review upcoming deadlines and appointments
+                                           ;; type "l" in the agenda to review logged items 
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/site.org")) (org-agenda-overriding-header "SITE:")))
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/arquivos/org/victor.org")) (org-agenda-overriding-header "GERAIS:"))))) ;; review waiting items 
+         ;; ...other commands here
+        ))

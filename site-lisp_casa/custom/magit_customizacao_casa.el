23635e9629aba1e468a95afb44b191357f996eeb
Victor Moreira <victorpolillo@hotmail.com>
2012-02-09 17:03:01 -0200

    Add mock_arrois aos repos magit

--- site-lisp_casa/custom/magit_customizacao_casa.el
+++ site-lisp_casa/custom/magit_customizacao_casa.el
@@ -1,1 +1,1 @@
-(setq magit-repo-dirs (quote ("/home/victor/arquivos/org" "/home/victor/arquivos/projetos/Space2Ctrl" "/home/victor/.emacs.d")))
+(setq magit-repo-dirs (quote ("/home/victor/arquivos/org" "/home/victor/arquivos/projetos/Space2Ctrl" "/home/victor/.emacs.d" "/home/victor/arquivos/projetos/arrois/mock_arrois")))

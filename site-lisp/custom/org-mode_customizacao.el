525ed13719358fbef49f9a5ea8a0c51049ba8c31
Victor Moreira <victorpolillo@hotmail.com>
2012-03-21 23:50:37 -0300

    Add. org-mode "EM ESPERA"

--- site-lisp/custom/org-mode_customizacao.el
+++ site-lisp/custom/org-mode_customizacao.el
@@ -14,7 +14,7 @@
 (setq org-agenda-dim-blocked-tasks 'invisible)
 ;; --------------------------------------------------
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "NEXT(n!)" "|" "FEITO(f)" "CANCELADO(c)")
+      '((sequence "EM ESPERA(e)" "TODO(t)" "NEXT(n!)" "|" "FEITO(f)" "CANCELADO(c)")
 	))
 
 (setq org-todo-keyword-faces

fc7711ab3c690deacf6b906819292e9e500f5ce5
Victor Moreira <victorpolillo@hotmail.com>
2012-03-31 23:50:52 -0300

    Add magit repo "arrois"

--- site-lisp_casa/custom/magit_customizacao_casa.el
+++ site-lisp_casa/custom/magit_customizacao_casa.el
@@ -1,1 +1,1 @@
-(setq magit-repo-dirs (quote ("/home/victor/arquivos/projetos/Space2Ctrl" "/home/victor/.emacs.d" "/home/victor/arquivos/projetos/arrois/Arrois")))
+(setq magit-repo-dirs (quote ("/home/victor/arquivos/projetos/Space2Ctrl" "/home/victor/.emacs.d" "/home/victor/arquivos/projetos/arrois/arrois")))

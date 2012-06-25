54d1c7316075f5e970e4b6560325050c1ea8f09b
Victor Moreira <victorpmoreira@gmail.com>
2012-06-25 17:13:46 -0300

    Clojure mode auto-complete funciona agora

--- site-lisp_casa/custom/clojure-mode_casa_custom.el
+++ site-lisp_casa/custom/clojure-mode_casa_custom.el
@@ -4,4 +4,4 @@
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
 
 (define-key clojure-mode-map (kbd "C-c C-j") 'clojure-jack-in)
-
+(define-key clojure-mode-map (kbd "M-TAB") 'slime-complete-symbol)

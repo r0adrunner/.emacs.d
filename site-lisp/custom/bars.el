feba321a41a418c9c0421946478d110b45645018
Victor Moreira <victorpmoreira@gmail.com>
2012-06-25 19:25:08 -0300

    Fix bars.el

--- site-lisp/custom/bars.el
+++ site-lisp/custom/bars.el
@@ -2,7 +2,7 @@
   (interactive)
   (progn
 ;;    (tool-bar-mode)
-    (menu-bar-mode) 
-    (scroll-bar-mode)))
+    (if menu-bar-mode (menu-bar-mode -1) (menu-bar-mode))
+    (if scroll-bar-mode (scroll-bar-mode -1) (scroll-bar-mode))))
 
 (global-set-key (kbd "s-f") 'barras-on-off)

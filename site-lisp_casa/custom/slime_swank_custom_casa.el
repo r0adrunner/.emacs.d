b0dcfe07a78a3ee8c0b8c238617fc50d4fc2c82b
Victor Moreira <victorpmoreira@gmail.com>
2012-06-25 22:49:25 -0300

    Fix: Cores no slime repl

--- site-lisp_casa/custom/slime_swank_custom_casa.el
+++ site-lisp_casa/custom/slime_swank_custom_casa.el
@@ -1,7 +1,7 @@
-(require 'slime)
-(require 'slime-repl)
 (require 'lisp_parens_custom)
 
+
+;;; Setup paredit:
 (defun setup-slime-repl-paredit ()
   (define-key slime-repl-mode-map
     "\C-j" 'delete-backward-char)  ; era slime-repl-newline-and-indent
@@ -26,11 +26,62 @@
 (add-hook 'slime-repl-mode-hook 'turn-on-paredit)
 (add-hook 'slime-repl-mode-hook 'setup-slime-repl-paredit)
 
-;;; Syntax highlighting
-(add-hook 'slime-repl-mode-hook
-          (defun clojure-mode-slime-font-lock ()
-            (let (font-lock-mode)
-              (clojure-mode-font-lock-setup))))
-
 ;;; Padrão utf-8 no repl
 (setq slime-net-coding-system 'utf-8-unix)
+
+
+;;; Cores no REPL:
+;;; Tirado de: https://gist.github.com/337280
+;;; all code in this function lifted from the clojure-mode function
+;;; from clojure-mode.el
+(defun clojure-font-lock-setup ()
+  (interactive)
+  (set (make-local-variable 'lisp-indent-function)
+       'clojure-indent-function)
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'clojure-doc-string-elt)
+  (set (make-local-variable 'font-lock-multiline) t)
+
+  (add-to-list 'font-lock-extend-region-functions
+               'clojure-font-lock-extend-region-def t)
+
+  (when clojure-mode-font-lock-comment-sexp
+    (add-to-list 'font-lock-extend-region-functions
+                 'clojure-font-lock-extend-region-comment t)
+    (make-local-variable 'clojure-font-lock-keywords)
+    (add-to-list 'clojure-font-lock-keywords
+                 'clojure-font-lock-mark-comment t)
+    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+
+  (setq font-lock-defaults
+        '(clojure-font-lock-keywords    ; keywords
+          nil nil
+          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+          nil
+          (font-lock-mark-block-function . mark-defun)
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
+
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            (font-lock-mode nil)
+            (clojure-font-lock-setup)
+            (font-lock-mode t)
+            ))
+
+(defadvice slime-repl-emit (after sr-emit-ad activate)
+  (with-current-buffer (slime-output-buffer)
+    (add-text-properties slime-output-start slime-output-end
+                         '(font-lock-face slime-repl-output-face
+                                          rear-nonsticky (font-lock-face)))))
+
+(defadvice slime-repl-insert-prompt (after sr-prompt-ad activate)
+  (with-current-buffer (slime-output-buffer)
+    (let ((inhibit-read-only t))
+      (add-text-properties slime-repl-prompt-start-mark (point-max)
+                           '(font-lock-face slime-repl-prompt-face
+                                            rear-nonsticky
+                                            (slime-repl-prompt
+                                             read-only
+                                             font-lock-face
+                                             intangible))))))

a81ba9f409ea829f3c60f6fc70974e79a012ca1d
Victor Moreira <victorpmoreira@gmail.com>
2012-08-15 22:05:33 -0300

    xdg-open => gvfs-open

--- site-lisp/custom/dired_customizacao.el
+++ site-lisp/custom/dired_customizacao.el
@@ -4,7 +4,7 @@
 (defun dired-open (&optional file-list)
   (interactive
    (list (dired-get-marked-files t current-prefix-arg)))
-  (apply 'call-process "xdg-open" nil 0 nil file-list))
+  (apply 'call-process "gvfs-open" nil 0 nil file-list))
 
 (define-key dired-mode-map (kbd "C-c C-c") 'dired-open)
 

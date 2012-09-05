8aa978f8ce637a6841659fd6a4b6a3addb74156e
Victor Moreira <victorpmoreira@gmail.com>
2012-09-05 18:27:50 -0300

    Workaround para problema com color-theme para emacs 24.

--- site-lisp_casa/init_casa.el
+++ site-lisp_casa/init_casa.el
@@ -18,6 +18,9 @@
 (require 'color-theme)
 (setq color-theme-is-global t)
 
+;; Color-theme package is broken in emacs 24. Quick fix:
+(defun plist-to-alist (plist) plist)
+
 (color-theme-deep-blue)
 
 ;; Fonte:

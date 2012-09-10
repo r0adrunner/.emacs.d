e8ca466299ad58c579448736c3a6bb6e3a51fc0c
Victor Moreira <victorpmoreira@gmail.com>
2012-09-10 12:32:27 -0300

    Add. lista de pacotes original do emacs

--- site-lisp/custom/package_custom.el
+++ site-lisp/custom/package_custom.el
@@ -1,3 +1,8 @@
 (require 'package)
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; Add the original Emacs Lisp Package Archive
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+;; Add the user-contributed repository
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))

8ea1b9970a59c6a24de33182a8411177580285e1
Victor Moreira <victorpmoreira@gmail.com>
2012-09-15 22:50:22 -0300

    Fix full-ack

--- site-lisp/custom/full-ack_customizacao.el
+++ site-lisp/custom/full-ack_customizacao.el
@@ -1,4 +1,4 @@
-;;(add-to-list 'load-path "./site-lisp/full-ack.el")
+(require 'full-ack)
 
 (autoload 'ack-same "full-ack" nil t)
 (autoload 'ack "full-ack" nil t)
@@ -9,20 +9,8 @@
 (setq ack-executable (or (executable-find "ack")
                          (executable-find "ack-grep")))
 
-;;Faz ele perguntar pelo diretório
+;; Faz o ack perguntar sempre pelo diretório
 (setq ack-prompt-for-directory t)
 
-;; Muda a lista de argumentos
-(defun ack-arguments-from-options (regexp)
-  (let ((arguments (list "--color"
-                         (ack-option "smart-case" (eq ack-ignore-case 'smart))
-                         (ack-option "heading" ack-heading)
-                         (ack-option "env" ack-use-environment))))
-    (unless ack-ignore-case
-      (push "-i" arguments))
-    ;; -a Faz ele procurar em todos os tipos de arquivo
-    (push "-a" arguments)
-    (unless regexp
-      (push "--literal" arguments))
-    (push (format "--context=%d" ack-context) arguments)
-    arguments))
+;; Procura em todos os tipos de arquivo
+(push "-a" ack-arguments)

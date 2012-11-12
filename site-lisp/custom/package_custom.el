251bd35755347fd02e6cd58895be577321a82696
Victor Moreira <victorpmoreira@gmail.com>
2012-11-11 22:20:26 -0200

    Fix package init script

--- site-lisp/custom/package_custom.el
+++ site-lisp/custom/package_custom.el
@@ -3,6 +3,9 @@
 ;; Add the original Emacs Lisp Package Archive
 (add-to-list 'package-archives
              '("elpa" . "http://tromey.com/elpa/"))
+
 ;; Add the user-contributed repository
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(package-initialize)

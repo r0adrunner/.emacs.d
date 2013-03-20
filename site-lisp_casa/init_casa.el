4725d8418c7b4f8f507e29f7203ae8a2c608457b
Victor Moreira <victorpmoreira@gmail.com>
2013-03-20 15:46:34 -0300

    Fix. browse-url-generic-args

--- site-lisp_casa/init_casa.el
+++ site-lisp_casa/init_casa.el
@@ -25,4 +25,5 @@
 
 ;; Browser default:
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "chromium")
+      browse-url-generic-program "chromium"
+      browse-url-generic-args nil)

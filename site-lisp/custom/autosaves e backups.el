7acd1338d87cbbcdfe729327c4b2f636e1348dc8
Victor Moreira <victorpmoreira@gmail.com>
2013-05-14 22:04:23 -0300

    Custom set variables todas definidas em init.el

--- site-lisp/custom/autosaves e backups.el
+++
@@ -1,4 +0,0 @@
-;; Put autosave files (ie #foo#) and backup files (ie foo~) in ~/.emacs.d/.
-(custom-set-variables
-  '(auto-save-file-name-transforms '((".*" "~/.emacs.d/autosaves/\\1" t)))
-  '(backup-directory-alist '((".*" . "~/.emacs.d/backups/"))))

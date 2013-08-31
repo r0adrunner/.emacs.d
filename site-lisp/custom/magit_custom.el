d3034b824cdb4a47ce3c7ecf0ebc8b28bd6fafe2
Victor Moreira <victorpmoreira@gmail.com>
2013-08-31 10:01:11 -0300

    Cores de fundo magit

--- site-lisp/custom/magit_custom.el
+++ site-lisp/custom/magit_custom.el
@@ -1,9 +1,9 @@
 (require 'magit)
 
-;; (eval-after-load 'magit
-;;   '(progn
-;;      (set-face-foreground 'magit-diff-add "green3")
-;;      (set-face-foreground 'magit-diff-del "red3")
-;;      (when (not window-system)
-;;        (set-face-background 'magit-item-highlight "black")
-;;        )))
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")
+     (when (not window-system)
+       (set-face-background 'magit-item-highlight "black")
+       )))

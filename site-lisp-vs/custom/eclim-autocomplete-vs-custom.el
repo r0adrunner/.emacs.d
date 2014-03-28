0f91bd062a6271de5af2eb23f76899d5877a8dec
Victor Moreira <victorpmoreira@gmail.com>
2014-03-28 11:13:01 -0300

    Ignorando 'ac-comphist.dat' do autocomplete

--- site-lisp-vs/custom/eclim-autocomplete-vs-custom.el
+++ site-lisp-vs/custom/eclim-autocomplete-vs-custom.el
@@ -13,11 +13,14 @@
   (let ((default-directory (concat (eclim--project-dir) "/")))
     (compile (concat "mvn -f " (eclim--maven-pom-path) " " command))))
 
+
 ;;; Auto complete mode:
 
 ;;regular auto-complete initialization
 (require 'auto-complete-config)
 (ac-config-default)
+
+(setq ac-comphist-file  "~/.emacs.d/ac-comphist.dat")
 
 ;; add the emacs-eclim source
 (require 'ac-emacs-eclim-source)

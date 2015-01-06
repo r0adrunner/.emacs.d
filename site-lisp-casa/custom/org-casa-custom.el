214c526c779282954545c9bb50a9183da5a680c0
Victor Moreira <victorpmoreira@gmail.com>
2015-01-06 19:15:03 -0200

    Rem. entrevistas.org

--- site-lisp-casa/custom/org-casa-custom.el
+++ site-lisp-casa/custom/org-casa-custom.el
@@ -1,4 +1,4 @@
-(setq org-agenda-files (quote ("~/org/victor.org" "~/org/entrevistas.org")))
+(setq org-agenda-files (quote ("~/org/victor.org")))
 
 ;; Org custom commands
 (setq org-agenda-custom-commands
@@ -10,13 +10,7 @@
 		      (org-agenda-repeating-timestamp-show-all t)
 		      (org-agenda-start-on-weekday nil)
 		      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("FEITO")))))
-	  ;; todo.org
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/entrevistas.org"))
-							      (org-agenda-overriding-header "ENTREVISTAS:")
-		;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))
-							      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
-							      ))
-	  ;; victor.org
+
 	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/victor.org"))
 							      (org-agenda-overriding-header "GERAIS:")
 		;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))

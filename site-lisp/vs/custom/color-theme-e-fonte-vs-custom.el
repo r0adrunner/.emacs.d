8bca1f1574f0f16c4f769668a7600d801185220d
Victor Moreira <victor.moreira@videosoft.com.br>
2015-03-05 16:28:08 -0300

    Usando zenburn em VS

--- site-lisp/vs/custom/color-theme-e-fonte-vs-custom.el
+++ site-lisp/vs/custom/color-theme-e-fonte-vs-custom.el
@@ -1,11 +1,13 @@
 ;; Color themes ---------------------------------------------------------
 
+(load-theme 'zenburn t)
+
 ;; Color theme init:
-(require 'color-theme)
-(setq color-theme-is-global t)
+;; (require 'color-theme)
+;; (setq color-theme-is-global t)
 
 ;; Color-theme package is broken in emacs 24. Quick fix:
-(defun plist-to-alist (plist) plist)
+;; (defun plist-to-alist (plist) plist)
 
 ;; (color-theme-classic)
 

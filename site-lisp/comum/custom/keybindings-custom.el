aa8a98d57896ef62c03a5f9c17003e258cfd9336
Victor Moreira <victorpmoreira@gmail.com>
2015-04-29 11:03:08 -0300

    Add keybinding C-; global

--- site-lisp/comum/custom/keybindings-custom.el
+++ site-lisp/comum/custom/keybindings-custom.el
@@ -143,5 +143,6 @@
 	 (comment-dwim nil)))
 
 (global-set-key (kbd "C-ç") 'comment-uncomment-line)  ; Era nada
+(global-set-key (kbd "C-;") 'comment-uncomment-line)  ; Era undo
 
 (global-set-key (kbd "C-f") nil)  ; Era forward-char

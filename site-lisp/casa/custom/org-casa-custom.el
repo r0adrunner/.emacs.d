2ffdded7c2094ac958e9598cbd6dfeaa38c4d07a
Victor <victorpmoreira@gmail.com>
2015-05-24 18:18:50 -0300

    Add. todo's kersplody ao org agenda

--- site-lisp/casa/custom/org-casa-custom.el
+++ site-lisp/casa/custom/org-casa-custom.el
@@ -1,4 +1,4 @@
-(setq org-agenda-files (quote ("~/org/victor.org" "~/org/cvs.org")))
+(setq org-agenda-files (quote ("~/org/victor.org" "~/org/kersplody.org")))
 
 ;; Org custom commands
 (setq org-agenda-custom-commands
@@ -10,13 +10,13 @@
 		      (org-agenda-repeating-timestamp-show-all t)
 		      (org-agenda-start-on-weekday nil)
 		      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("FEITO")))))
-	  ;; todo.org
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/cvs.org"))
-							      (org-agenda-overriding-header "ENTREVISTAS:")
-		;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))
-							      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
-							      ))
-	  ;; victor.org
+	  ;; Arquivo org secundario
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/kersplody.org"))
+	  						      (org-agenda-overriding-header "KERSPLODY:")
+	  	;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))
+	  						      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
+	  						      ))
+	  ;; Arquivo org primario
 	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/victor.org"))
 							      (org-agenda-overriding-header "GERAIS:")
 		;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))

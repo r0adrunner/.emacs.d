39ae62310dc321ccdc777b248aeaf294639d9726
Victor <victorpmoreira@gmail.com>
2015-07-24 14:49:20 -0300

    Mudado browser padrão em casa

--- site-lisp/casa/custom/outros-casa-custom.el
+++ site-lisp/casa/custom/outros-casa-custom.el
@@ -1,4 +1,4 @@
 ;; Browser default:
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "chromium"
+      browse-url-generic-program "firefox"
       browse-url-generic-args nil)

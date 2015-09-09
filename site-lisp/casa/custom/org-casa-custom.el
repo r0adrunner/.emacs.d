564b717b9dccf43a3c86fd4e60aab1e0e2719def
Victor <victorpmoreira@gmail.com>
2015-09-08 22:49:42 -0300

    Rem. kersp agenda

--- site-lisp/casa/custom/org-casa-custom.el
+++ site-lisp/casa/custom/org-casa-custom.el
@@ -1,4 +1,4 @@
-(setq org-agenda-files (quote ("~/org/victor.org" "~/org/kersplody.org")))
+(setq org-agenda-files (quote ("~/org/victor.org")))
 
 ;; Org custom commands
 (setq org-agenda-custom-commands
@@ -10,12 +10,7 @@
 		      (org-agenda-repeating-timestamp-show-all t)
 		      (org-agenda-start-on-weekday nil)
 		      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("FEITO")))))
-	  ;; Arquivo org secundario
-	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/kersplody.org"))
-	  						      (org-agenda-overriding-header "KERSPLODY:")
-	  	;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))
-	  						      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
-	  						      ))
+
 	  ;; Arquivo org primario
 	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/victor.org"))
 							      (org-agenda-overriding-header "GERAIS:")

09731f2b26acf217f09713e9f3803fdcd76e99e1
Victor <vpmcvs@gmail.com>
2017-10-17 00:06:11 -0200

    Change color theme and font size

--- site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
+++ site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
@@ -1,6 +1,6 @@
 ;; Color themes ---------------------------------------------------------
 
-;; (load-theme 'zenburn t)
+(load-theme 'zenburn t)
 
 ;; Color theme init:
 (require 'color-theme)
@@ -9,12 +9,13 @@
 ;; Color-theme package is broken in emacs 24. Quick fix:
 (defun plist-to-alist (plist) plist)
 
-(color-theme-deep-blue)
+;; (color-theme-deep-blue)
+(color-theme-zenburn)
 
 ;; Fontes -----------------------------------------------------------------
-;; (set-face-attribute 'default nil :height 143 :family "Inconsolata")
+;; (set-face-attribute 'default nil :height 170 :family "Inconsolata")
 ;; (set-face-attribute 'default nil :height 120 :family "DejaVu Sans Mono")
-(set-face-attribute 'default nil :height 190 :family "DejaVu Sans Mono")
+(set-face-attribute 'default nil :height 180 :family "DejaVu Sans Mono")
 
 (defun enlarge-font ()
   (interactive)

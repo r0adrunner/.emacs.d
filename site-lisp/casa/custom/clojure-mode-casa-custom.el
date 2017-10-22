11b9f5f3b8316c44e5c5b4a178d42d26b9f397cc
Victor <vpmcvs@gmail.com>
2017-10-22 00:35:48 -0200

    Add. clj-refactor

--- site-lisp/casa/custom/clojure-mode-casa-custom.el
+++ site-lisp/casa/custom/clojure-mode-casa-custom.el
@@ -1,5 +1,14 @@
 (require 'clojure-mode)
 (require 'clojure-mode-extra-font-locking)
 (require 'lisp_parens_custom)
+(require 'clj-refactor)
+
+(defun clj-refactor-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'clj-refactor-clojure-mode-hook)
 
 (add-hook 'clojure-mode-hook 'turn-on-paredit)

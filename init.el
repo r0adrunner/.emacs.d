7a3d69d90e8c2007f57bd6506c334591a844ec87
Victor <vpmcvs@gmail.com>
2018-09-11 00:36:18 -0300

    Blinking cursor

--- init.el
+++ init.el
@@ -14,7 +14,8 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
- '(blink-cursor-mode nil)
+ '(blink-cursor-mode 1)
+ '(blink-cursor-blinks 0)
  '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

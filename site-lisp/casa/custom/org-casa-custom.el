d8088755050e7e0efcabdd933fe8a6baae476803
Victor <vpmcvs@gmail.com>
2018-12-17 23:15:32 -0200

    config. org

--- site-lisp/casa/custom/org-casa-custom.el
+++ site-lisp/casa/custom/org-casa-custom.el
@@ -1,4 +1,4 @@
-(setq org-agenda-files (quote ("~/org/victor.org")))
+(setq org-agenda-files (quote ("~/org/victor.org" "~/org/apartamentos.org" "~/projetos/trading/docs/tradingtodo.org")))
 
 ;; Org custom commands
 (setq org-agenda-custom-commands
@@ -14,8 +14,24 @@
 	  ;; Arquivo org primario
 	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/victor.org"))
 							      (org-agenda-overriding-header "GERAIS:")
-		;					      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("TODO")))
+							      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("IDEIA")))
 							      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
-							      ))))
+							      ))
+
+	  ;; Arquivo org aps
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/org/apartamentos.org"))
+							      (org-agenda-overriding-header "APARTAMENTOS:")
+							      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("IDEIA")))
+							      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
+							      ))
+
+	  ;; Arquivo org trading
+	  (tags-todo "SCHEDULED=\"\"|SCHEDULED<=\"<today>\"" ((org-agenda-files '("~/projetos/trading/docs/tradingtodo.org"))
+							      (org-agenda-overriding-header "TRADING:")
+							      (org-agenda-skip-function '(org-agenda-skip-entry-if 'todo '("IDEIA")))
+							      (org-agenda-sorting-strategy '(todo-state-down priority-down)) ; Ordena por prioridade e todo keyword
+							      ))
+	  )
+	 )
 	)
-)
+      )

fc9f0c21f5fb692b6796792383040e5e06e1827a
Victor <vpmcvs@gmail.com>
2018-12-22 20:25:06 -0200

    Change color scheme + font

--- site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
+++ site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
@@ -1,6 +1,6 @@
 ;; Color themes ---------------------------------------------------------
 
-(load-theme 'zenburn t)
+;; (load-theme 'zenburn t)
 
 ;; Color theme init:
 (require 'color-theme)
@@ -9,11 +9,11 @@
 ;; Color-theme package is broken in emacs 24. Quick fix:
 (defun plist-to-alist (plist) plist)
 
-;; (color-theme-deep-blue)
-(color-theme-zenburn)
+(color-theme-deep-blue)
+;; (color-theme-zenburn) ;; ativar 'load-theme' acima
 
 ;; Fontes -----------------------------------------------------------------
-(set-face-attribute 'default nil :height 130 :family "Inconsolata")
+;; (set-face-attribute 'default nil :height 130 :family "Inconsolata")
 ;; (set-face-attribute 'default nil :height 120 :family "DejaVu Sans Mono")
 ;; (set-face-attribute 'default nil :height 180 :family "DejaVu Sans Mono")
 

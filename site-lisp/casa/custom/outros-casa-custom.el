3d110ccaf4daf552d10ec860ca525b89a9a2fa8b
Victor <vpmcvs@gmail.com>
2019-02-28 15:28:08 -0300

    Changed default browser

--- site-lisp/casa/custom/outros-casa-custom.el
+++ site-lisp/casa/custom/outros-casa-custom.el
@@ -1,4 +1,5 @@
 ;; Browser default:
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "firefox"
+      browse-url-generic-program "chromium-browser"
+      ;; browse-url-generic-args (list "--incognito")
       browse-url-generic-args nil)

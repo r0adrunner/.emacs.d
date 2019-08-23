72eb8c18296374d096993826a27a108fd2167510
Victor <vpmcvs@gmail.com>
2019-08-23 01:01:58 -0300

    Fix. magit keybindings

--- site-lisp/comum/custom/magit-custom.el
+++ site-lisp/comum/custom/magit-custom.el
@@ -8,3 +8,6 @@
        ;; (set-face-background 'magit-item-highlight "black")
        )))
 
+(with-eval-after-load 'magit-status
+  (define-key magit-status-mode-map (kbd "C-i") 'magit-section-cycle) ;;era 'magit-section-toggle
+  (define-key magit-status-mode-map (kbd "C-<tab>") nil)) ;; era 'magit-section-cycle

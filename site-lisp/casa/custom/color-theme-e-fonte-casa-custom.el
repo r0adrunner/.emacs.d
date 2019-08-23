61f77f42b6c705a3848c90748698271b25f068c7
Victor <vpmcvs@gmail.com>
2019-08-23 18:53:37 -0300

    Mod. fonte

--- site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
+++ site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
@@ -4,10 +4,9 @@
 ;; temas serão carregados em init.el->custom-set-variables
 
 ;; Fontes -----------------------------------------------------------------
-;; (set-face-attribute 'default nil :height 120)
+;; (set-face-attribute 'default nil :height 140)
 ;; (set-face-attribute 'default nil :height 140 :family "Terminus")
-;; (set-face-attribute 'default nil :height 130 :family "Inconsolata")
-(set-face-attribute 'default nil :height 140 :family "DejaVu Sans Mono")
-;; (set-face-attribute 'default nil :height 180 :family "DejaVu Sans Mono")
+;; (set-face-attribute 'default nil :height 140 :family "Inconsolata")
+(set-face-attribute 'default nil :height 120 :family "DejaVu Sans Mono")
 
 ;; Enlarge font: C-x C-+

77782f3ffa5343ae1e23d1b1a6abac6211281c28
Victor <vpmcvs@gmail.com>
2019-09-18 17:39:42 -0300

    Rem magit process C-tab

--- site-lisp/comum/custom/magit-custom.el
+++ site-lisp/comum/custom/magit-custom.el
@@ -15,3 +15,9 @@
 (with-eval-after-load 'magit-revision
   (define-key magit-revision-mode-map (kbd "C-i") 'magit-section-cycle) ;;era 'magit-section-toggle
   (define-key magit-revision-mode-map (kbd "C-<tab>") nil)) ;; era 'magit-section-cycle
+
+(with-eval-after-load 'magit-process
+  (define-key magit-process-mode-map (kbd "C-i") 'magit-section-cycle) ;;era 'magit-section-toggle
+  (define-key magit-process-mode-map (kbd "C-<tab>") nil)) ;; era 'magit-section-cycle
+
+(global-set-key (kbd "<C-tab>") 'other-window)

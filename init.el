60ab1e9b516ab5fddba9ddb702c1b0aff367330a
Victor <vpmcvs@gmail.com>
2020-02-29 16:31:04 -0300

    Add. rjsx mode

--- init.el
+++ init.el
@@ -49,7 +49,7 @@
  '(objed-cursor-color "#C16069")
  '(package-selected-packages
    (quote
-    (beacon neotree projectile csv-mode solarized-theme elpy uniquify-files doom-themes color-theme-modern cyberpunk-theme zenburn-theme zenburn yaml-mode s rainbow-mode php-mode pager org markdown-mode magit jabber haskell-mode git-rebase-mode git-commit-mode full-ack ess csharp-mode clojure-mode-extra-font-locking clj-refactor auto-complete)))
+    (rjsx-mode beacon neotree projectile csv-mode solarized-theme elpy uniquify-files doom-themes color-theme-modern cyberpunk-theme zenburn-theme zenburn yaml-mode s rainbow-mode php-mode pager org markdown-mode magit jabber haskell-mode git-rebase-mode git-commit-mode full-ack ess csharp-mode clojure-mode-extra-font-locking clj-refactor auto-complete)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(vc-annotate-background "#323334")
  '(vc-annotate-color-map

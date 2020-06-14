79633b96180ac24f4a2efeae10a3dc5111251b8c
Victor <vpmcvs@gmail.com>
2020-06-14 20:47:58 -0300

    custom color

--- init.el
+++ init.el
@@ -120,4 +120,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(clojure-keyword-face ((t (:inherit font-lock-keyword-face))))
  '(region ((t (:background "gainsboro" :foreground "#002b36")))))

a7080616cc265ae0f1f3366f0fa7b438138f3894
Victor <vpmcvs@gmail.com>
2020-06-27 01:38:58 -0300

    updated browser

--- site-lisp/casa/custom/varios-casa-custom.el
+++ site-lisp/casa/custom/varios-casa-custom.el
@@ -6,7 +6,7 @@
 ;; Browser default:
 ;; ========================================
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "chromium-browser"
+      browse-url-generic-program "palemoon"
       ;; browse-url-generic-args (list "--incognito")
       browse-url-generic-args nil)
 

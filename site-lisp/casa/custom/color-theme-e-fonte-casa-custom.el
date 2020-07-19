c94077b5cd85a7b16fd98254cf5507df4e586292
Victor <vpmcvs@gmail.com>
2020-07-19 08:04:09 -0300

    Change font too

--- site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
+++ site-lisp/casa/custom/color-theme-e-fonte-casa-custom.el
@@ -6,7 +6,7 @@
 ;; Fontes -----------------------------------------------------------------
 ;; (set-face-attribute 'default nil :height 140)
 ;; (set-face-attribute 'default nil :height 140 :family "Terminus")
-;; (set-face-attribute 'default nil :height 140 :family "Inconsolata")
-(set-face-attribute 'default nil :height 120 :family "DejaVu Sans Mono")
+(set-face-attribute 'default nil :height 140 :family "Inconsolata")
+;; (set-face-attribute 'default nil :height 120 :family "DejaVu Sans Mono")
 
 ;; Enlarge font: C-x C-+
